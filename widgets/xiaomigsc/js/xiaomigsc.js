593900c75fec737b3d694da51235b76d02373d5b
ioBroker Adapter Creator <noreply@iobroker.net>
2024-08-13 16:06:39 +0000

    Initial commit by adapter creator

---
+++ widgets/xiaomigsc/js/xiaomigsc.js
@@ -0,0 +1,71 @@
+/*
+	ioBroker.vis xiaomigsc Widget-Set
+
+	version: "0.0.1"
+
+	Copyright 2024 gumica-star gumica-star@wir-schmidt.de
+*/
+"use strict";
+
+/* global $, vis, systemDictionary */
+
+// add translations for edit mode
+$.extend(true, systemDictionary, {
+	// Add your translations here, e.g.:
+	// "size": {
+	// 	"en": "Size",
+	// 	"de": "Größe",
+	// 	"ru": "Размер",
+	// 	"pt": "Tamanho",
+	// 	"nl": "Grootte",
+	// 	"fr": "Taille",
+	// 	"it": "Dimensione",
+	// 	"es": "Talla",
+	// 	"pl": "Rozmiar",
+	// 	"zh-cn": "尺寸"
+	// }
+});
+
+// this code can be placed directly in xiaomigsc.html
+vis.binds["xiaomigsc"] = {
+	version: "0.0.1",
+	showVersion: function () {
+		if (vis.binds["xiaomigsc"].version) {
+			console.log("Version xiaomigsc: " + vis.binds["xiaomigsc"].version);
+			vis.binds["xiaomigsc"].version = null;
+		}
+	},
+	createWidget: function (widgetID, view, data, style) {
+		var $div = $("#" + widgetID);
+		// if nothing found => wait
+		if (!$div.length) {
+			return setTimeout(function () {
+				vis.binds["xiaomigsc"].createWidget(widgetID, view, data, style);
+			}, 100);
+		}
+
+		var text = "";
+		text += "OID: " + data.oid + "</div><br>";
+		text += 'OID value: <span class="xiaomigsc-value">' + vis.states[data.oid + ".val"] + "</span><br>";
+		text += 'Color: <span style="color: ' + data.myColor + '">' + data.myColor + "</span><br>";
+		text += "extraAttr: " + data.extraAttr + "<br>";
+		text += "Browser instance: " + vis.instance + "<br>";
+		text += 'htmlText: <textarea readonly style="width:100%">' + (data.htmlText || "") + "</textarea><br>";
+
+		$("#" + widgetID).html(text);
+
+		// subscribe on updates of value
+		function onChange(e, newVal, oldVal) {
+			$div.find(".template-value").html(newVal);
+		}
+		if (data.oid) {
+			vis.states.bind(data.oid + ".val", onChange);
+			//remember bound state that vis can release if didnt needed
+			$div.data("bound", [data.oid + ".val"]);
+			//remember onchange handler to release bound states
+			$div.data("bindHandler", onChange);
+		}
+	},
+};
+
+vis.binds["xiaomigsc"].showVersion();
